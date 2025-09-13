c04751ffb796c6fb2a5c14f4b47513cc695e1ea1
Grace Odongo <graceakinyi024@gmail.com>
2025-09-13 12:35:59 +0300

    updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers=function(){
+    let drivers=['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.splice(0,2)
+    
+}
+const returnLastTwoDrivers=function(){
+    let driver=['Antonia', 'Nuru', 'Amari', 'Mo']
+    return driver.splice(2)
+}
+const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(integer){
+ return function(fare){
+    return fare * integer
+ }
+}
+const fareDoubler=createFareMultiplier(2)
+
+const fareTripler=createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, fn) {
+   
+  return fn(drivers);
+
+}
+
+
+console.log(returnFirstTwoDrivers())
+console.log(returnLastTwoDrivers())
+console.log(selectingDrivers())
+console.log(selectingDrivers([0]))
+console.log(selectingDrivers([1]))
+console.log(createFareMultiplier())
+console.log(fareDoubler())
+console.log(fareTripler())
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
